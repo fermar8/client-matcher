220f97dfda83b2e542f5ec3eecea172f95552431
Ferran Martinez <ferran.ms8@gmail.com>
2021-06-05 19:26:47 +0200

    Afegit autocompletar a formulari champs

--- src/Pages/Modo.js
+++ src/Pages/Modo.js
@@ -1,11 +1,16 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Form, FormGroup, Label, 
     Container, Row, Col, Input } from 'reactstrap';
+import Select from 'react-select';
 import { Link } from 'react-router-dom';
 import { RedButton } from './../Components/RedButton';
 import './../styles/modo.css';
 
 function Modo() {
+
+    const [ champs, setChamps ] = useState([]);
+    const [ myChamps, setMyChamps ] = useState([]);
+    const [ prefChamps, setPrefChamps ] = useState([]);
 
     const [ page, setPage ] = useState(1);
 
@@ -29,93 +34,79 @@ function Modo() {
     const [ siCheck, setSiCheck ] = useState(false);
     const [ noCheck, setNoCheck ] = useState(false);
 
+const getChamps=()=>{
+        fetch('champs.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            return response.json();
+          })
+          .then(function(myJson) {
+            setChamps(myJson)
+          });
+      }
 
-    const inputDuo = useRef();
-    const inputFlex = useRef();
-    const inputClash = useRef();
-    const inputOtro = useRef();
+useEffect(()=>{
+    getChamps()
+    console.log('champs', champs)
+},[])
 
-    const input4fun = useRef();
-    const inputTryhard = useRef();
-    const inputProbar = useRef();
-    const inputOtps = useRef();
-
-    const inputTop = useRef();
-    const inputJungle = useRef();
-    const inputMid = useRef();
-    const inputBot = useRef();
-    const inputSupp = useRef();
-    const inputFill = useRef();
-
-    const inputSi = useRef();
-    const inputNo = useRef();
 
 const handleClick = (e) => {
     if (e.target.value === "inputDuo") {
     setDuoCheck(!duoCheck);
-    inputDuo.current.click();
     };
 
     if (e.target.value === "inputFlex") {
         setFlexCheck(!flexCheck);
-        inputFlex.current.click();
     };
 
     if (e.target.value === "inputClash") {
         setClashCheck(!clashCheck);
-        inputClash.current.click();
     };
     if (e.target.value === "inputOtro") {
         setOtroCheck(!otroCheck);
-        inputOtro.current.click();
     }
     if (e.target.value === "input4fun") {
         setForFunCheck(!forFunCheck);
-        input4fun.current.click();
     }
     if (e.target.value === "inputTryhard") {
         setTryhardCheck(!tryhardCheck);
-        inputTryhard.current.click();
     }
     if (e.target.value === "inputProbar") {
         setProbarCheck(!probarCheck);
-        inputProbar.current.click();
     }
     if (e.target.value === "inputOtps") {
         setOtpsCheck(!otpsCheck);
-        inputOtps.current.click();
     }
     if (e.target.value === "inputTop") {
         setTopCheck(!topCheck);
-        inputTop.current.click();
     }
     if (e.target.value === "inputJungle") {
         setJungleCheck(!jungleCheck);
-        inputJungle.current.click();
     }
     if (e.target.value === "inputMid") {
         setMidCheck(!midCheck);
-        inputMid.current.click();
     }
     if (e.target.value === "inputBot") {
         setBotCheck(!botCheck);
-        inputBot.current.click();
     }
     if (e.target.value === "inputSupp") {
         setSuppCheck(!suppCheck);
-        inputSupp.current.click();
     }
     if (e.target.value === "inputFill") {
         setFillCheck(!fillCheck);
-        inputFill.current.click();
     }
     if (e.target.value === "inputSi") {
         setSiCheck(!siCheck);
-        inputSi.current.click();
     }
     if (e.target.value === "inputNo") {
         setNoCheck(!noCheck);
-        inputNo.current.click();
     }
 }
 
@@ -159,24 +150,20 @@ const handleClick = (e) => {
                     <FormGroup className="modo-modo">
                     <Label for="duo">
                         <button className={duoCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputDuo" onClick={handleClick}>Duo</button>
-                        <input className="modo-input" type="checkbox" ref={inputDuo}/>
                     </Label>
 
                     <Label for="flex">
                         <button className={flexCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputFlex" onClick={handleClick}>Flex</button>
-                        <input className="modo-input" type="checkbox" ref={inputFlex}/>
                     </Label>
                 </FormGroup>
 
                 <FormGroup className="modo-modo">
                     <Label for="clash">
                         <button className={clashCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputClash" onClick={handleClick}>Clash</button>
-                        <input className="modo-input" type="checkbox" ref={inputClash}/>
                     </Label>
 
                     <Label for="otro">
                         <button className={otroCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputOtro" onClick={handleClick}>Otro</button>
-                        <input className="modo-input" type="checkbox" ref={inputOtro}/>
                     </Label>
                 </FormGroup> 
                 </>
@@ -187,24 +174,20 @@ const handleClick = (e) => {
                     <FormGroup className="modo-modo">
                     <Label for="4fun">
                         <button className={forFunCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="input4fun" onClick={handleClick}>4fun</button>
-                        <input className="modo-input" type="checkbox" ref={input4fun}/>
                     </Label>
 
                     <Label for="tryhard">
                         <button className={tryhardCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputTryhard" onClick={handleClick}>Tryhard</button>
-                        <input className="modo-input" type="checkbox" ref={inputTryhard}/>
                     </Label>
                 </FormGroup>
 
                 <FormGroup className="modo-modo">
                     <Label for="probar">
                         <button className={probarCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputProbar" onClick={handleClick}>Probar champs</button>
-                        <input className="modo-input" type="checkbox" ref={inputProbar}/>
                     </Label>
 
                     <Label for="otps">
                         <button className={otpsCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputOtps" onClick={handleClick}>OTP's</button>
-                        <input className="modo-input" type="checkbox" ref={inputOtps}/>
                     </Label>
                 </FormGroup> 
                 </>
@@ -215,36 +198,30 @@ const handleClick = (e) => {
                 <FormGroup className="modo-modo">
                     <Label for="top">
                         <button className={topCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputTop" onClick={handleClick}>Top</button>
-                        <input className="modo-input" type="checkbox" ref={inputTop}/>
                     </Label>
 
                     <Label for="jungle">
                         <button className={jungleCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputJungle" onClick={handleClick}>Jungle</button>
-                        <input className="modo-input" type="checkbox" ref={inputJungle}/>
                     </Label>
                 </FormGroup>
 
                 <FormGroup className="modo-modo">
                     <Label for="mid">
                         <button className={midCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputMid" onClick={handleClick}>Mid</button>
-                        <input className="modo-input" type="checkbox" ref={inputMid}/>
                     </Label>
 
                     <Label for="bot">
                         <button className={botCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputBot" onClick={handleClick}>Bot</button>
-                        <input className="modo-input" type="checkbox" ref={inputBot}/>
                     </Label>
                 </FormGroup> 
 
                 <FormGroup className="modo-modo">
                     <Label for="supp">
                         <button className={suppCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputSupp" onClick={handleClick}>Supp</button>
-                        <input className="modo-input" type="checkbox" ref={inputSupp}/>
                     </Label>
 
                     <Label for="fill">
                         <button className={fillCheck ? "modo-btn-sel" : "modo-btn"} type="button" value="inputFill" onClick={handleClick}>Fill</button>
-                        <input className="modo-input" type="checkbox" ref={inputFill}/>
                     </Label>
                 </FormGroup> 
                 </>
@@ -252,23 +229,61 @@ const handleClick = (e) => {
 
                 {page === 4 ? 
                 <FormGroup>
+
                     <Label className="modo-4text" for="mi-champ">Quiero jugar:</Label>
-                    <Input className="modo-4input" type="text"></Input>
+                    <Select
+                        className="modo-4input"
+                        isMulti
+                        name="myChamps"
+                        onChange={setMyChamps}
+                        options={champs}
+                        defaultOptions
+                        placeholder={"Introduce tus campeones preferidos"}
+                        theme={theme => ({
+                            ...theme,
+                            borderRadius: 0,
+                            colors: {
+                            ...theme.colors,
+                            primary25: '#FF4655',
+                            primary: 'black',
+                            neutral0: '#fafafa',
+                            neutral90: '#fafafa'
+                        },
+                        })}
+                     />
+            
 
                     <Label className="modo-4text" for="team-champ">Prefiero que mis compañer@s jueguen:</Label>
-                    <Input className="modo-4input" type="text"></Input>
+                    <Select
+                         className="modo-4input"
+                         isMulti
+                         name="prefChamps"
+                         onChange={setPrefChamps}
+                         options={champs}
+                         defaultOptions
+                         placeholder={"Introduce los campeones que prefieres que tus compañeros jueguen"}
+                         theme={theme => ({
+                            ...theme,
+                            borderRadius: 0,
+                            colors: {
+                            ...theme.colors,
+                            primary25: '#FF4655',
+                            primary: 'black',
+                            neutral0: '#fafafa',
+                            neutral90: '#fafafa'
+                        },
+                        })}
+                     />
 
                     <p className="modo-rango">Solo quiero ver compañeros de mi rango o superior</p>
                     
                 <div className="modo-sino">
                     <Label for="mid">
                         <button className={siCheck ? "btn-sel" : "btn-sino"} type="button" value="inputSi" onClick={handleClick}>Sí</button>
-                        <input className="modo-input" type="checkbox" ref={inputSi}/>
                     </Label>
 
                     <Label for="bot">
                         <button className={noCheck ? "btn-sel" : "btn-sino"} type="button" value="inputNo" onClick={handleClick}>No</button>
-                        <input className="modo-input" type="checkbox" ref={inputNo}/>
                     </Label>
                 </div>
 
