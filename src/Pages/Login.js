1fc542d0d1a10fd7d1aad0bbf30e8e0d3c84225c
Marcus Fifth <marc9quintanilla@gmail.com>
2021-06-09 18:32:00 +0200

    login casi acabat

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,37 +1,146 @@
-import React from 'react'
+import React, { useState, useRef, Component }  from 'react'
 import { Form, FormGroup, Label, Input,
      Container, Row, Col } from 'reactstrap';
 import imgPerfil from './../images/img-perfil.png';
+//import { withAuth } from './../context/auth-context';
 import { RedButton } from './../Components/RedButton';
+import AuthService from "../services/auth.service";
 import './../styles/login.css';
 
+const required = value => {
+    if (!value) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          This field is required!
+        </div>
+      );
+    }
+  };
+  
+  export default class Login extends Component {
+    constructor(props) {
+      super(props);
+      this.handleLogin = this.handleLogin.bind(this);
+      this.onChangeUsername = this.onChangeUsername.bind(this);
+      this.onChangePassword = this.onChangePassword.bind(this);
+  
+      this.state = {
+        username: "",
+        password: "",
+        loading: false,
+        message: ""
+      };
+    }
 
-function Login() {
-    return (
-        <Container fluid={true} style={{height: "100vh", width: "100%"}}>
-        <Row>
-        <Col xs="12" className="login-flex">
-        <h1 className="login-title">INICIA SESIÓN</h1>
-        <img className="login-img" alt="foto-perfil" src={imgPerfil}></img>
-        <Form>
-            <FormGroup>
-                <Label className="login-label">Correo electrónico</Label>
-                <Input className="login-input" type="text" placeholder="Introduce tu correo electrónico"></Input>
-            </FormGroup>
-            <FormGroup>
-                <Label className="login-label">Contraseña</Label>
-                <Input className="login-input" type="password" placeholder="Introduce tu contraseña"></Input>
-            </FormGroup>
+    onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        });
+      }
+    
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        });
+      }
+    
+      handleLogin(e) {
+        e.preventDefault();
+    
+        this.setState({
+          message: "",
+          loading: true
+        });
+
+
+        AuthService.login(this.state.username, this.state.password).then(
+            () => {
+            this.props.setloguejat(true);
+                this.props.history.push("/landing");
             
-            <FormGroup className="login-button">
-            <RedButton>Inicia sesión</RedButton>
-            </FormGroup>
+            },
+            error => {
+              const resMessage =
+                (error.response &&
+                  error.response.data &&
+                  error.response.data.message) ||
+                error.message ||
+                error.toString();
+    
+              this.setState({
+                loading: false,
+                message: resMessage
+              });
+            }
+          );
+    }
+    
 
-        </Form>
-        </Col>
-        </Row>
-        </Container>
-    )
+        /*if (this.checkBtn.context._errors.length === 0) {
+            AuthService.login(this.state.username, this.state.password).then(
+              () => {
+                this.props.history.push("/profile");
+                window.location.reload();
+              },
+              error => {
+                const resMessage =
+                  (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                  error.message ||
+                  error.toString();
+      
+                this.setState({
+                  loading: false,
+                  message: resMessage
+                });
+              }
+            );
+          } else {
+            this.setState({
+              loading: false
+            });
+          }
+        }*/
+  
+   
+    render() {
+        return (
+            <Container fluid={true} style={{height: "100vh", width: "100%"}}>
+            <Row>
+            <Col xs="12" className="login-flex">
+            <h1 className="login-title">INICIA SESIÓN</h1>
+            <img className="login-img" alt="foto-perfil" src={imgPerfil}></img>
+            <Form onSubmit={this.handleLogin}
+            ref={c => {
+              this.form = c;
+            }}>
+                <FormGroup>
+                    <Label className="login-label">Correo electrónico</Label>
+                    <Input className="login-input" type="text" value={this.state.username}
+                onChange={this.onChangeUsername}
+                 name="username" placeholder="Introduce tu correo electrónico"></Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label className="login-label">Contraseña</Label>
+                    <Input value={this.state.password}
+                onChange={this.onChangePassword}
+                 className="login-input" type="password" name="password" placeholder="Introduce tu contraseña"></Input>
+                </FormGroup>
+                
+                <FormGroup className="login-button">
+                <RedButton>Inicia sesión</RedButton>
+                </FormGroup>
+    
+            </Form>
+            </Col>
+            </Row>
+            </Container>
+        )
+    }
 }
+    
 
-export default Login
+
+
+
---
+++ src/context/auth-context.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import authService from './../services/auth.service';
+
+const { Consumer, Provider } = React.createContext();
+
+
+class AuthProvider extends React.Component {
+  state = {
+    isLoggedIn: false,
+    isLoading: true,
+    user: null, 
+    error: false
+  }
+
+  componentDidMount () {
+    authService.me()
+     .then((user) => this.setState({ isLoggedIn: true, user: user, isLoading: false }))
+     .catch((err) => this.setState({ isLoggedIn: false, user: null, isLoading: false }));
+  }
+
+  signup = (username, team, email, password) => {
+    authService.signup(username, team, email, password )
+      .then((user) => this.setState({ isLoggedIn: true, user }) )
+      .catch((err) => {
+        this.setState({ isLoggedIn: false, user: null, error: err.response.data.message });
+      })
+  }
+
+  login = (username, password) => {
+    authService.login( username, password )
+      .then((user) => this.setState({ isLoggedIn: true, user }))
+      .catch((err) => {
+        this.setState({ isLoggedIn: false, user: null, error: err.response.data.message });
+      })
+  }
+
+  logout = () => {
+    authService.logout()
+      .then(() => this.setState({ isLoggedIn: false, user: null }))
+      .catch((err) => console.log(err));
+  }
+
+
+  render() {
+    const { isLoggedIn, isLoading, user, error } = this.state;
+    const { signup, login, logout } = this;
+
+    if (isLoading) return <p>Loading</p>;
+
+    return(
+      <Provider value={{ isLoggedIn, isLoading, user, signup, login, logout, error }}  >
+        {this.props.children}
+      </Provider>
+    )
+  }
+
+}
+
+
+// HOC that converts regular component into a Consumer
+const withAuth = (WrappedComponent) => {
+  
+  return class extends React.Component {
+    render() {
+      return(
+        <Consumer>
+          { (value) => {
+            const { isLoggedIn, isLoading, user, signup, login, logout, error } = value;
+
+            return (<WrappedComponent 
+                      {...this.props}
+                      isLoggedIn={isLoggedIn} 
+                      isLoading={isLoading} 
+                      user={user} 
+                      signup={signup} 
+                      login={login} 
+                      logout={logout}
+                      error={error}
+                    />)
+
+          } }
+        </Consumer>
+        )
+    }
+}
+}
+
+
+export { AuthProvider, withAuth }
